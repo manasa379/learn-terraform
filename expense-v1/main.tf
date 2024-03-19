11dbf02c97d9fde6fafa35a17c39ff8be2e9acc3
DELL <manasajalagam379@gmail.com>
2024-03-19 21:27:40 +0530

    security group

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-0f3c7d07486cad139"
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-05b0d4c09e67606a4"]
   tags = {
@@ -16,7 +16,7 @@ resource "aws_route53_record" "frontend" {
 }
 
 resource "aws_instance" "backend" {
-  ami           = "ami-0f3c7d07486cad139"
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-05b0d4c09e67606a4"]
   tags = {
@@ -33,7 +33,7 @@ resource "aws_route53_record" "backend" {
 }
 
 resource "aws_instance" "mysql" {
-  ami           = "ami-0f3c7d07486cad139"
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-05b0d4c09e67606a4"]
   tags = {
