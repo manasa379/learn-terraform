e807bdc56f2d5cd7d44f940d95c1fb0f12acb8f0
DELL <manasajalagam379@gmail.com>
2024-03-19 19:36:56 +0530

    security group

--- 02-data/main.tf
+++ 02-data/main.tf
@@ -1,7 +1,7 @@
-data "aws_ami" "ami"  {
+data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["212103609741"]
+  owners           = [ "973714476881"]
 }
 
 data "aws_security_group" "sg" {
