a982b8ad4fc83b6a9c3e22c94a5d1fcf668dc8e5
DELL <manasajalagam379@gmail.com>
2024-03-20 09:03:44 +0530

    Expense provisioner

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -7,15 +7,6 @@ resource "aws_instance" "frontend" {
   }
 }
 
-  provisioner "local-exec" {
-    command = <<EOF
-cd/home/centos/infra-ansible
-gitpull
-sleep 60
-ansible-playbook -i ${self.private_id}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
-EOF
-  }
-
 resource "aws_route53_record" "frontend" {
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = "frontend.${var.zone_id}"
@@ -23,7 +14,16 @@ resource "aws_route53_record" "frontend" {
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
-
+resource "null_resource" "frontend" {
+  provisioner "local-exec" {
+    command = <<EOF
+cd/home/centos/infra-ansible
+gitpull
+sleep 60
+ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+EOF
+  }
+}
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
@@ -41,6 +41,17 @@ resource "aws_route53_record" "backend" {
   records = [aws_instance.backend.private_ip]
 }
 
+resource "null_resource" "backend" {
+  provisioner "local-exec" {
+    command = <<EOF
+cd/home/centos/infra-ansible
+gitpull
+sleep 60
+ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+EOF
+  }
+}
+
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
@@ -56,4 +67,15 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.mysql.private_ip]
+}
+
+resource "null_resource" "mysql" {
+  provisioner "local-exec" {
+    command = <<EOF
+cd/home/centos/infra-ansible
+gitpull
+sleep 60
+ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+EOF
+  }
 }
