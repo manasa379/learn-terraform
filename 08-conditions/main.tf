ed0c1c4deb4212888aa51b45a9fe8edcd380ed2e
DELL <manasajalagam379@gmail.com>
2024-05-02 15:35:33 +0530

    conditions in terraform

--- 08-conditions/main.tf
+++ 08-conditions/main.tf
@@ -1,8 +1,7 @@
-resource "aws_instance" "test" {
+resource "aws_instance" "example" {
   ami           = "ami-03265a0778a880afb"
   instance_type = var.instance_type == null ? "t3.medium" : var.instance_type
 }
-
 variable "instance_type" {
   default = null
 }
